c47cc15e4ad45db975ace9a9cf5fae33cc5ac6fe
JLannoo <julianlannoo@gmail.com>
2021-10-31 11:59:40 -0300

    Add "no product" message when no products are retrieved

--- src/components/ProductContainer/ProductContainer.js
+++ src/components/ProductContainer/ProductContainer.js
@@ -1,17 +1,20 @@
 import Product from "../Product/Product.js";
-import "./productContainer.css";
 import {Row, Col} from "react-bootstrap";
 
 
-export default function ProdudctContainer({products}){
+export default function ProdudctContainer({products, filterFn}){
+    
     return (
         <div className="product-container">
             <Row style={{justifyContent: 'space-evenly'}}>
-                {products.map(product => (
-                    <Col xs={12} sm={6} md={3} key={product.id}>
-                        <Product product={product} />
-                    </Col>
-                ))}
+                {products.length === 0 ? 
+                    <h3>No products have been found :(</h3> : 
+                    products.filter((a) => filterFn(a)).map(product => (
+                        <Col key={product.id} xs={12} sm={6} md={4} lg={3}>
+                            <Product product={product}/>
+                        </Col>
+                    ))
+                }
             </Row>
         </div>
     )
