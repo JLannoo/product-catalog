6fcee2591589d101d3cdbb3c3d70f51de97d7ef0
JLannoo <julianlannoo@gmail.com>
2021-10-31 17:52:49 -0300

    Add functionality to Edit

--- src/components/Edit/Edit.js
+++ src/components/Edit/Edit.js
@@ -1,5 +1,150 @@
+import React from 'react';
+import Async from 'react-async';
+import Forms from 'react-bootstrap/Form';
+import {Row, Col, Button} from 'react-bootstrap';
+import ProdudctContainer from "../ProductContainer/ProductContainer.js";
+import Loading from '../Loading/Loading.js';
+
+const URL = "http://localhost:3001"
+
+async function fetchData() {
+    const response = await fetch(`${URL}/products/`)
+
+    if(!response.ok) {
+        throw new Error(response.statusText);
+    }
+    const data = await response.json();
+    return data;
+}
+
 export default function Edit(props){
+    const [editingView, setEditingView] = React.useState(false);
+    const [product, setProduct] = React.useState({});
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setProduct({...product, [name]: value})
+    }
+       
     return (
-        <h1>Edit</h1>
+        <div className="edit-container"> 
+            {!editingView ? (
+                <>
+                    <h3>Which product would you like to edit?</h3>
+                    <Async promiseFn={fetchData}>
+                        <Async.Pending> 
+                            <Loading/> 
+                        </Async.Pending>
+
+                        <Async.Fulfilled>
+                            {products => 
+                                <>
+                                    <ProdudctContainer 
+                                        products={products} 
+                                        type="edit" 
+                                        editFn={(product)=>{
+                                            setEditingView(true);
+                                            setProduct(product);
+                                        }}/>
+                                </>
+                            }  
+                        </Async.Fulfilled>
+
+                        <Async.Rejected>
+                            {"There seems to have been an issue, please try again later!"}
+                        </Async.Rejected>
+                    </Async>
+                </>
+            ) : (
+                <>
+                <Forms>
+                    <Forms.Group controlId="formBasicName">
+                            <Forms.Label>Name</Forms.Label>
+                            <Forms.Control type="text" placeholder="Enter name" name="name" value={product.name} onChange={(e) => handleChange(e)}/>
+                        </Forms.Group>
+
+                        <Forms.Group controlId="formBasicDescription">
+                            <Forms.Label>Description</Forms.Label>
+                            <Forms.Control type="text" placeholder="Enter description" name="description" value={product.description} onChange={(e) => handleChange(e)}/>
+                        </Forms.Group>
+
+                        <Forms.Group controlId="formBasicPrice">
+                            <Row>
+                                <Col>
+                                <Forms.Label>Dollars</Forms.Label>
+                                <Forms.Control type="text" placeholder="Enter dollars" name="price_dollar" value={product.price_dollar} onChange={(e) => handleChange(e)}/>
+                                </Col>
+                                <Col>
+                                <Forms.Label>Cents</Forms.Label>
+                                <Forms.Control type="text" placeholder="Enter cents" name="price_cents" value={product.price_cents} onChange={(e) => handleChange(e)}/>
+                                </Col>
+                            </Row>
+                        </Forms.Group>
+
+                        <Forms.Group controlId="formBasicImage">
+                            <Forms.Label>Image URL</Forms.Label>
+                            <Forms.Control type="text" placeholder="Enter image URL" name="image" value={product.image} onChange={(e) => handleChange(e)}/>
+                        </Forms.Group>
+                    </Forms>
+
+                    <Button variant="warning" type="button" onClick={(e) => setEditingView(false)}>
+                            Back
+                    </Button>
+                    <Button variant="primary" type="button" onClick={(e) => editProduct(product)}>
+                            Submit
+                    </Button>
+                    
+                </>
+            )
+            } 
+        </div>
     )
 }
+
+async function editProduct({id, name, description, price_cents, price_dollar, image}){
+    console.log(id, name, description, price_cents, price_dollar, image);
+
+    if(name !== ""  && description !== ""  && price_cents !== "" && price_dollar !== ""  && image !== ""){
+        const response = await fetch(`${URL}/products/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name,
+                description,
+                price_cents,
+                price_dollar,
+                image
+            })
+        })
+
+        if(response.ok){
+            alert("Product successfully edited!")
+            window.location.reload();
+        } else {
+            alert("There seems to have been an issue, please try again later!")
+        }
+    } else {
+        alert("Please fill out all fields!")
+    }
+}
+
+{/* <h3>Which product would you like to edit?</h3>
+                <Async promiseFn={fetchData}>
+                    <Async.Pending> 
+                        <Loading/> 
+                    </Async.Pending>
+
+                    <Async.Fulfilled>
+                        {products => 
+                            <>
+                                <ProdudctContainer products={products} type="edit" editFn={editProduct}/>
+                            </>
+                        }  
+                    </Async.Fulfilled>
+
+                    <Async.Rejected>
+                        {"There seems to have been an issue, please try again later!"}
+                    </Async.Rejected>
+                </Async> */}
--- src/components/ProductContainer/ProductContainer.js
+++ src/components/ProductContainer/ProductContainer.js
@@ -26,13 +26,26 @@ export default function ProdudctContainer({type="catalog", products, filterFn = 
                             <h3>No products have been found :(</h3> : 
                             products.map(product => (
                                 <Col key={product.id} xs={12} sm={6} md={4} lg={3}>
-                                    <Product product={product} onClick={()=>{deleteFn(product.id)}}/>
+                                    <Product product={product} onClick={()=>{deleteFn(product.id, product.name)}}/>
                                 </Col>
                             ))
                         }
                     </Row>
             );
         case "edit":
+            return (
+                <Row style={{justifyContent: 'space-evenly'}}>
+                        {products.length === 0 ? 
+                            <h3>No products have been found :(</h3> : 
+                            products.map(product => (
+                                <Col key={product.id} xs={12} sm={6} md={4} lg={3}>
+                                    <Product product={product} onClick={() => {editFn(product)}}/>
+                                </Col>
+                            ))
+                        }
+                    </Row>
+            );
+        default:
             return null;
     }
 }
