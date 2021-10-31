99159e347068781bf47cafd9a626d174c825f71e
JLannoo <julianlannoo@gmail.com>
2021-10-31 20:01:16 -0300

    Update ProductContainer.js

--- src/components/ProductContainer/ProductContainer.js
+++ src/components/ProductContainer/ProductContainer.js
@@ -2,16 +2,17 @@ import Product from "../Product/Product.js";
 import {Row, Col} from "react-bootstrap";
 
 
-export default function ProdudctContainer({type="catalog", products, filterFn = ()=> true, deleteFn = ()=>{}, editFn = ()=>{}}) {
+export default function ProdudctContainer({type="catalog", products, deleteFn = ()=>{}, editFn = ()=>{}}) {
     switch(type){
         case "catalog":
             return (
                 <div className="product-container">
                     <Row style={{justifyContent: 'space-evenly'}}>
                         {products.length === 0 ? 
-                            <h3>No products have been found :(</h3> : 
-                            products.filter((a) => filterFn(a)).map(product => (
-                                <Col key={product.id} xs={12} sm={6} md={4} lg={3}>
+                            <h3>No products have been found :(</h3> 
+                                : 
+                            products.map(product => (
+                                <Col key={product.id} >
                                     <Product product={product}/>
                                 </Col>
                             ))
