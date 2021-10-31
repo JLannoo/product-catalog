62d1eff04dd5a5af05da29a1740114baedc7e2b2
JLannoo <julianlannoo@gmail.com>
2021-10-31 12:00:40 -0300

    Add functionality to AddProduct section

--- src/components/Add/Add.js
+++ src/components/Add/Add.js
@@ -1,28 +1,41 @@
 import React from "react"
 import Forms from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import { Row, Col } from 'react-bootstrap'
+
+const URL = "http://localhost:3001/products/"
 
 export default function Add(props){
     const [name, setName] = React.useState('');
     const [description, setDescription] = React.useState('');
-    const [price, setPrice] = React.useState('');
+    const [priceDollar, setPriceDollar] = React.useState('');
+    const [priceCents, setPriceCents] = React.useState('');
     const [image, setImage] = React.useState('');
 
     return (
         <div className="add">
-            <Forms onSubmit={props.handleSubmit}>
+            <Forms>
                 <Forms.Group controlId="formBasicName">
                     <Forms.Label>Name</Forms.Label>
-                    <Forms.Control type="text" placeholder="Enter name" name="name" value={props.name} onChange={(e)=>setName(e.target.value)}/>
+                    <Forms.Control type="text" placeholder="Enter name" name="name" value={name} onChange={(e)=>setName(e.target.value)}/>
                 </Forms.Group>
 
                 <Forms.Group controlId="formBasicDescription">
                     <Forms.Label>Description</Forms.Label>
-                    <Forms.Control type="text" placeholder="Enter description" name="description" value={props.description} onChange={(e)=>setDescription(e.target.value)}/>
+                    <Forms.Control type="text" placeholder="Enter description" name="description" value={description} onChange={(e)=>setDescription(e.target.value)}/>
                 </Forms.Group>
 
                 <Forms.Group controlId="formBasicPrice">
-                    <Forms.Label>Price</Forms.Label>
-                    <Forms.Control type="text" placeholder="Enter price" name="price" value={props.price} onChange={(e)=>setPrice(e.target.value)}/>
+                    <Row>
+                        <Col>
+                        <Forms.Label>Dollars</Forms.Label>
+                        <Forms.Control type="text" placeholder="Enter dollars" name="price" value={priceDollar} onChange={(e)=>setPriceDollar(e.target.value)}/>
+                        </Col>
+                        <Col>
+                        <Forms.Label>Cents</Forms.Label>
+                        <Forms.Control type="text" placeholder="Enter cents" name="price" value={priceCents} onChange={(e)=>setPriceCents(e.target.value)}/>
+                        </Col>
+                    </Row>
                 </Forms.Group>
 
                 <Forms.Group controlId="formBasicImage">
@@ -30,6 +43,39 @@ export default function Add(props){
                     <Forms.Control type="text" placeholder="Enter image URL" name="image" value={props.image} onChange={(e)=>setImage(e.target.value)}/>
                 </Forms.Group>
             </Forms>
+
+            <Button variant="primary" type="button" onClick={(e) => AddProduct(name, description, priceDollar, priceCents, image)}>
+                    Submit
+            </Button>
         </div>
     )
+}
+
+async function AddProduct(name, description, priceDollar, priceCents, image){
+    while(priceCents > 99){
+        priceCents -= 100;
+        priceDollar++;  
+    }
+
+    const response = await fetch(URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            description: description,
+            price_dollar: priceDollar,
+            price_cents: priceCents,
+            image: image
+        })
+    });
+    const data = await response.json();
+
+    if(response.ok === 200){
+        alert("Product added!");
+    }
+    else{
+        alert("Error adding product!\n" + "Please check all field are completed");
+    }
 }
